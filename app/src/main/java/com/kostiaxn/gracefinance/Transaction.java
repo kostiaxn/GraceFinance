87ccf1021f536c56cbcc76e925d8a92d1ce8c958
Kostia Savchenko <kostia.xn@gmail.com>
2023-03-01 11:54:18 +0300

    Transaction.java

---
+++ app/src/main/java/com/kostiaxn/gracefinance/Transaction.java
@@ -0,0 +1,13 @@
+package com.kostiaxn.gracefinance;
+
+abstract public class Transaction {
+    private int id;
+    private String accountName;
+    private String cardName;
+    private double amount;
+    private String place;
+    private String comment;
+    private String currency;
+    private double exchangeRate;
+
+}
