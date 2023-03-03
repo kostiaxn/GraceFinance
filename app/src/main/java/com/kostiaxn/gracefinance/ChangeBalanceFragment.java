4a546ebe38fc296b68d7b19e8c8a5a240fc84e4a
Kostia Savchenko <kostia.xn@gmail.com>
2023-03-03 10:11:37 +0300

    Обновление MainActivity после сохранения новых данных о балансе

--- app/src/main/java/com/kostiaxn/gracefinance/ChangeBalanceFragment.java
+++ app/src/main/java/com/kostiaxn/gracefinance/ChangeBalanceFragment.java
@@ -3,21 +3,26 @@ package com.kostiaxn.gracefinance;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 public class ChangeBalanceFragment extends DialogFragment {
+    TextView tvCurrentBalanceAmount;
 
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
         AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.changebalancedialog, null);
+
         return builder
-               .setTitle("Введите новый баланс")
-                .setView(R.layout.changebalancedialog)
+                .setTitle("Введите новый баланс")
+                .setView(view)
                 .setPositiveButton("Сохранить", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -27,10 +32,10 @@ public class ChangeBalanceFragment extends DialogFragment {
                         double newBalance = Double.parseDouble(newBalanceString); // convert string value to double
                         balanceUpdater.balance = newBalance; // set the new balance value
                         balanceUpdater.setBalance(); // call the method
+                        ((MainActivity) getActivity()).tvCurrentBalanceAmount.setText(String.valueOf(newBalance));
                     }
                 })
                 .setNegativeButton("Отмена", null)
                 .create();
-
     }
 }
